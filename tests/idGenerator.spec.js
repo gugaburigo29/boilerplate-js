17ed11ada7bad55261590920b5a3f2ba42d3d406
Guga <guga.burigo29@outlook.com>
2019-01-30 08:09:24 -0200

    Inicia boilerplate setup para TDD

---
+++ tests/idGenerator.spec.js
@@ -0,0 +1,11 @@
+import {expect} from 'chai'
+
+describe('Test', () => {
+
+    it('should running', () => {
+
+        expect(true).to.equal(true);
+
+    });
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, 'src/js/index.js'),
+        path.resolve(__dirname, 'src/js/IdGenerator.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    watch: true,
+    mode: "development"
+}
